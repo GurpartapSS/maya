ff220a0785486018a42c489bd64d9e7e5e0306ce
Gurpartap_SS <gurpartapsingh.sarkaria@gmail.com>
2022-01-28 01:18:26 -0500

    fix for audio buffer

--- deepspeechtest.py
+++ deepspeechtest.py
@@ -3,19 +3,41 @@ import pyaudio
 import numpy as np
 from halo import Halo
 Model_path = 'Models/deepspeech-0.9.3-models.tflite'
-# scorer_path = 'Models/deepspeech-0.9.3-models.scorer'
-f = deepspeech.Model(Model_path)
-# f.enableExternalScorer(scorer_path=scorer_path)
+scorer_path = 'Models/deepspeech-0.9.3-models.scorer'
+model = deepspeech.Model(Model_path)
+model.enableExternalScorer(scorer_path=scorer_path)
+model.beamWidth = 500
+
+import matplotlib.pyplot as plt 
+
+def plotAudio2(output):
+        fig, ax = plt.subplots(nrows=1,ncols=1, figsize=(20,4))
+        plt.plot(output, color='blue')
+        ax.set_xlim((0, len(output)))
+        plt.show()
+
+st = model.createStream()
+speech = ''
+# To handle stream data
+def callback(in_data, frame_count, time_info, status):
+    global speech
+    data = np.frombuffer(in_data,np.int16)
+    st.feedAudioContent(data)
+
 
 Rate = 16000
 Chunk_size = 1024
+channels  = 1
 
 pa = pyaudio.PyAudio()
 
 stream = None
 frames = []
 try:
-    stream = pa.open(format = pyaudio.paInt16,channels=1,rate=Rate,frames_per_buffer=Chunk_size,input=True)
+    stream = pa.open(format = pyaudio.paInt16,
+    channels=channels,rate=Rate,
+    frames_per_buffer=Chunk_size,input=True)
+    # stream_callback=callback)
     print("Listening ..")
     while(True):
         data = stream.read(Chunk_size)
@@ -26,15 +48,17 @@ except KeyboardInterrupt:
 stream.close()
 
 spinner = Halo('Loading ..')
-st = f.createStream()
 for frame in frames:
     if spinner: spinner.start()
     st.feedAudioContent(frame)
+    print(st.intermediateDecode())
 
 text = st.finishStream()
 spinner.succeed(text)
 
-# print("Detected ",text)
+print("Detected ",text)
 
 if stream is not None:
     stream.close()
+
+# plotAudio2(frames)
