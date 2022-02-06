868811c1598df216620ede2ca077be8bbcfda354
GurpartapSS <gurpartapsingh.sarkaria@gmail.com>
2022-02-06 09:24:31 +0000

    Fixed face detection with correct box positioning

--- faceDetection_coral.py
+++ faceDetection_coral.py
@@ -1,16 +1,17 @@
+# All Models available @ https://coral.ai/models/all/
+
 import cv2
 import tflite_runtime.interpreter as tflite
 import numpy as np
 
 class faceCoral:
     def __init__(self):
-        self.interpreter = tflite.Interpreter("face-detector-quantized_edgetpu.tflite",experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+        # self.interpreter = tflite.Interpreter("Models/face-detector-quantized_edgetpu.tflite",experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+        self.interpreter = tflite.Interpreter("Models/ssd_mobilenet_v2_face_quant_postprocess_edgetpu.tflite",experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
         self.interpreter.allocate_tensors()
-        self.input_details = self.interpreter.get_input_details() #list of details
-        self.output_details = self.interpreter.get_output_details() #list of details
+        self.input_details = self.interpreter.get_input_details() #list of input details -- expected size of input
+        self.output_details = self.interpreter.get_output_details() #list of output details -- Index #0 Boxes #1 Categories #2 Scores
         self.scoreTHreshold = 0.7
-
-        self.sigmoidScoreThreshold = np.log(self.scoreTHreshold/(1-self.scoreTHreshold))
 
 
     def main(self):
@@ -20,36 +21,24 @@ class faceCoral:
             ret, frame = cap.read()
             if not ret:
                 break
-        #    img = frame
             inpH = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            inpW = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            inpW = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img,(128,128))
-            # print(img.shape)
+            img = cv2.resize(img,(320,320))
             input_data = np.expand_dims(img, axis=0)
             self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
             self.interpreter.invoke()
-            # signatures = interpreter.get_signature_list()
-            # print(signatures)
-            # interpreter.get_tensor(output['index']).shape
             boxes = np.squeeze(self.interpreter.get_tensor(self.output_details[0]['index']))
-            scores = np.squeeze(self.interpreter.get_tensor(self.output_details[1]['index']))
-            facesReq = np.where(scores > self.sigmoidScoreThreshold)[0]
-            newScores = 1.0/(1.0 + np.exp(-scores[facesReq]))
+            scores = np.squeeze(self.interpreter.get_tensor(self.output_details[2]['index']))
             for i in range(len(scores)):
                 if (scores[i] > self.scoreTHreshold) and (scores[i] < 1.0):
-                    ymin = int(max(1,(boxes[i][0] * 300)))
-                    xmin = int(max(1,(boxes[i][1] * 300)))
-                    ymax = int(min(inpH,(boxes[i][2] * 300)))
-                    xmax = int(min(inpW,(boxes[i][3] * 300)))
+                    ymin = int(max(1,(boxes[i][0] * inpH)))
+                    xmin = int(max(1,(boxes[i][1] * inpW)))
+                    ymax = int(min(inpH,(boxes[i][2] * inpH)))
+                    xmax = int(min(inpW,(boxes[i][3] * inpW)))
                     print(ymin, xmin, ymax, xmax)
                     cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
 
-            # Extract inform /ation of filtered detections
-            # boxes, keypoints = extractDetections(output0, goodDetectionsIndices)
-
-            # Filter results with non-maximum suppression
-            # detectionResults = filterWithNonMaxSupression(boxes, keypoints, scores)
             cv2.imshow('image',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
