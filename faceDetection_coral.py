9a14e2ad4910739de35480df8dd776dc0e925771
Gavy0197 <gurpartapsingh.sarkaria@gmail.com>
2022-02-19 21:22:44 -0500

    Adding Face mmotion detection [still needs improvement in face datection in low light]

--- faceDetection_coral.py
+++ faceDetection_coral.py
@@ -21,8 +21,8 @@ class faceCoral:
             ret, frame = cap.read()
             if not ret:
                 break
-            inpH = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            inpW = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            self._inpH = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            self._inpW = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             img = cv2.resize(img,(320,320))
             input_data = np.expand_dims(img, axis=0)
@@ -32,16 +32,29 @@ class faceCoral:
             scores = np.squeeze(self.interpreter.get_tensor(self.output_details[2]['index']))
             for i in range(len(scores)):
                 if (scores[i] > self.scoreTHreshold) and (scores[i] < 1.0):
-                    ymin = int(max(1,(boxes[i][0] * inpH)))
-                    xmin = int(max(1,(boxes[i][1] * inpW)))
-                    ymax = int(min(inpH,(boxes[i][2] * inpH)))
-                    xmax = int(min(inpW,(boxes[i][3] * inpW)))
-                    print(ymin, xmin, ymax, xmax)
+                    ymin = int(max(1,(boxes[i][0] * self._inpH)))
+                    xmin = int(max(1,(boxes[i][1] * self._inpW)))
+                    ymax = int(min(self._inpH,(boxes[i][2] * self._inpH)))
+                    xmax = int(min(self._inpW,(boxes[i][3] * self._inpW)))
+                    # print(ymin, xmin, ymax, xmax)
+                    face_coord = [ymin, xmin, ymax, xmax]
+                    self.detect_movement(face_coord)
                     cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
 
             cv2.imshow('image',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
+
+    def detect_movement(self, coord):
+        if(coord[1] < self._inpW / 4):
+            print("Moving left")
+            print(coord[1], self._inpW/4)
+        elif(coord[3] > self._inpW * .75):
+            print("Moving right")
+            print(coord[3], self._inpW*3/4)
+        pass
+
 
 if __name__ == '__main__':
     f = faceCoral()
