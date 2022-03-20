2a50f578c42a009053581c6af445083dbf8967d3
GurpartapSS <gurpartapsingh.sarkaria@gmail.com>
2022-03-20 21:38:13 +0000

    fixing repeated movement comand hanndling

--- soundCmdListener.py
+++ soundCmdListener.py
@@ -5,6 +5,7 @@ import actuatorControl
 
 class localListener:
   def __init__(self):
+      self.__running = False
       self.s = socket.socket()        
       print ("Socket successfully created")
     
@@ -30,9 +31,14 @@ class localListener:
                   data = data.decode('utf-8')
                   print('Received data: ',data)
                   if(int(data) <= 10):
+                        if self.__running is False:
+                              self.__running = True
+                        else:
+                              self.act.movement_stop()
                         self.act.movement_auto(1)
                   if(int(data) == 12):
                         self.act.movement_stop()
+                        self.__running = False
 
 if __name__ == "__main__":
   l = localListener()
